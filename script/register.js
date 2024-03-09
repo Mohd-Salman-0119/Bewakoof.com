d794e1b25ea893fafb9635fc6d2d3df6b8ba0b04
Mohd-Salman-0119 <salmanansari910550@gmail.com>
2024-03-09 15:25:26 +0530

    url: added

--- script/register.js
+++ script/register.js
@@ -6,21 +6,23 @@
 const BASE_URL = 'https://bewakoof-web-clone.vercel.app/api'
 
 
-document.querySelector('.loginForm').addEventListener('submit', (event) => {
-     event.preventDefault()
+document.querySelector('.registerForm').addEventListener('submit', (event) => {
+     event.preventDefault();
+     const name = document.querySelector('.inputName').value;
      const email = document.querySelector('.inputEmail').value;
      const password = document.querySelector('.inputPassword').value;
+     console.log(name, email, password)
+     alert()
 
-     fetch(`${BASE_URL}/login`, {
+     fetch(`${BASE_URL}/register`, {
           method: 'POST',
           headers: {
                'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ email, password, name }),
      }).then(response => response.json())
           .then(data => {
                // Assuming the server responds with a token property
-               const token = data.token;
                console.log(data)
 
                if (token) {
